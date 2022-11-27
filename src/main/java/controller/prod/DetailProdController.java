3b533f7fcdb1d3a245a8d852d6167371341883d5
kthrc <106314022+kthrc@users.noreply.github.com>
2022-11-27 16:58:16 +0900

    1658

---
+++ src/main/java/controller/prod/DetailProdController.java
@@ -0,0 +1,36 @@
+package controller.prod;
+
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import controller.Controller;
+import controller.user.UserSessionUtils;
+import model.service.ProdManager;
+import model.service.UserManager;
+import model.Product;
+import model.User;
+
+public class DetailProdController implements Controller {
+    private static final Logger log = LoggerFactory.getLogger(DetailProdController.class);
+
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response)	throws Exception {
+        String id = UserSessionUtils.getLoginUserId(request.getSession());
+        UserManager manager = UserManager.getInstance();
+        //int prodId = 
+
+		//request.setAttribute("prodId", prodId);
+        return "/.jsp";
+    }
+    
+   
+}
--- src/main/java/controller/user/LoginController.java
+++ src/main/java/controller/user/LoginController.java
@@ -8,28 +8,28 @@ import controller.Controller;
 import model.service.UserManager;
 
 public class LoginController implements Controller {
-    @Override
-    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-       String userId = request.getParameter("userId");
-      String userPw = request.getParameter("userPw");
-      
-      try {
-         // 모델에 로그인 처리를 위임
-         UserManager manager = UserManager.getInstance();
-         manager.login(userId, userPw);
-   
-         // 세션에 사용자 이이디 저장
-         HttpSession session = request.getSession();
-            session.setAttribute(UserSessionUtils.USER_SESSION_KEY, userId);
-            
-            return "redirect:/";         
-      } catch (Exception e) {
-         /* UserNotFoundException이나 PasswordMismatchException 발생 시
-          * 다시 login form을 사용자에게 전송하고 오류 메세지도 출력
-          */
-            request.setAttribute("loginFailed", true);
-         request.setAttribute("exception", e);
-            return "/user/loginForm.jsp";         
-      }   
-    }
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String userId = request.getParameter("userId");
+		String userPw = request.getParameter("userPw");
+
+		try {
+			// 모델에 로그인 처리를 위임
+			UserManager manager = UserManager.getInstance();
+			manager.login(userId, userPw);
+
+			// 세션에 사용자 이이디 저장
+			HttpSession session = request.getSession();
+			session.setAttribute(UserSessionUtils.USER_SESSION_KEY, userId);
+
+			return "redirect:/";         
+		} catch (Exception e) {
+			/* UserNotFoundException이나 PasswordMismatchException 발생 시
+			 * 다시 login form을 사용자에게 전송하고 오류 메세지도 출력
+			 */
+			request.setAttribute("loginFailed", true);
+			request.setAttribute("exception", e);
+			return "/user/loginForm.jsp";         
+		}   
+	}
 }
