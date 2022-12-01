2b93af05c911bfe19382fae72829d90bf281d10f
DDWU <DDWU@B303-38>
2022-12-01 14:55:22 +0900

    gngn

--- src/main/java/controller/reserve/InsertReserveController.java
+++ src/main/java/controller/reserve/InsertReserveController.java
@@ -44,24 +44,18 @@ public class InsertReserveController implements Controller {
 	
 	public static Date transformDate(String date)
     {
-        SimpleDateFormat beforeFormat = new SimpleDateFormat("yyyymmdd");
-        
-        // Date로 변경하기 위해서는 날짜 형식을 yyyy-mm-dd로 변경해야 한다.
+        SimpleDateFormat beforeFormat = new SimpleDateFormat("yyyy-mm-dd");
         SimpleDateFormat afterFormat = new SimpleDateFormat("yyyy-mm-dd");
         
         java.util.Date tempDate = null;
         
         try {
-            // 현재 yyyymmdd로된 날짜 형식으로 java.util.Date객체를 만든다.
             tempDate = beforeFormat.parse(date);
         } catch (ParseException e) {
             e.printStackTrace();
         }
         
-        // java.util.Date를 yyyy-mm-dd 형식으로 변경하여 String로 반환한다.
         String transDate = afterFormat.format(tempDate);
-        
-        // 반환된 String 값을 Date로 변경한다.
         Date d = Date.valueOf(transDate);
         
         return d;
---
+++ src/main/java/controller/reserve/ViewReserveFormController.java
@@ -0,0 +1,28 @@
+package controller.reserve;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.Controller;
+import controller.user.UserSessionUtils;
+import model.Product;
+import model.User;
+import model.service.ProdManager;
+import model.service.UserManager;
+
+public class ViewReserveFormController implements Controller{
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String id = UserSessionUtils.getLoginUserId(request.getSession());
+		UserManager manager = UserManager.getInstance();
+		User user = manager.findUser(id);
+		request.setAttribute("user", user);
+		
+		int prodId = Integer.parseInt(request.getParameter("productId"));
+		ProdManager prodManager = ProdManager.getInstance();
+		Product product = prodManager.findProduct(prodId);
+		request.setAttribute("product", product);
+		
+
+		return "/reserve/reserve.jsp";
+	}
+}
