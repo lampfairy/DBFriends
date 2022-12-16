7672dbddb882474d27f62b2d66b791ae03f43014
lampfairy <s01041574286@gmail.com>
2022-12-17 02:52:34 +0900

    ss

--- src/main/java/controller/main/GotoMyPageController.java
+++ src/main/java/controller/main/GotoMyPageController.java
@@ -8,11 +8,13 @@ import controller.user.UserSessionUtils;
 import model.User;
 import model.service.UserManager;
 import model.Reservation;
+import model.Review;
 import model.service.BookmarkManager;
 import model.service.ProdManager;
 import model.Bookmark;
 import model.Product;
 import model.service.ReserveManager;
+import model.service.ReviewManager;
 
 import java.sql.Date;
 import java.text.ParseException;
@@ -28,75 +30,72 @@ public class GotoMyPageController implements Controller {
         if (!UserSessionUtils.hasLogined(request.getSession())) {
             return "redirect:/user/loginForm";     // login form 요청으로 redirect
         }
-        
+
         /*
         String currentPageStr = request.getParameter("currentPage");    
         int currentPage = 1;
         if (currentPageStr != null && !currentPageStr.equals("")) {
             currentPage = Integer.parseInt(currentPageStr);
         }       
-        */
-        
+         */
+
         // List<User> userList = manager.findUserList(currentPage, countPerPage);
 
         // userList 객체와 현재 로그인한 사용자 ID를 request에 저장하여 전달 
         String id = UserSessionUtils.getLoginUserId(request.getSession());
         UserManager manager = UserManager.getInstance();
         User user = manager.findUser(id);
-        
+
         BookmarkManager boma = BookmarkManager.getInstance();
         List<Bookmark> bookmarkList = boma.findUserBookmarkList(id);
         request.setAttribute("bookmarkList", bookmarkList); 
-        
-        ReserveManager rema = ReserveManager.getInstance();
-        List<Reservation> reservationList = rema.findReservationList(id);
+
+        ReserveManager reservema = ReserveManager.getInstance();
+        List<Reservation> reservationList = reservema.findReservationList(id);
         request.setAttribute("user", user);
-        
-        List<Reservation> reserveList = new  ArrayList<Reservation>();
-        List<Reservation> visitedList = new  ArrayList<Reservation>();
-        
+
+        List<Reservation> reserveList = new ArrayList<Reservation>();
+        List<Reservation> visitedList = new ArrayList<Reservation>();
+
         if(reservationList != null) {
             LocalDateTime now = LocalDateTime.now();
             String formatedNow = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
             Date nowDate = transformDate(formatedNow);
-            
+
             for(int i = 0; i < reservationList.size(); i++) {
-                Reservation reservation = rema.findReservation(reservationList.get(i).getReservationId());
+                Reservation reservation = reservema.findReservation(reservationList.get(i).getReservationId());
                 if(reservation.getEndDate().compareTo(nowDate) < 0) {
                     visitedList.add(reservation);
                 }
-                else {
+                else {  
                     reserveList.add(reservation);
                 }
             }
         }
-        else {  
-            request.setAttribute("reserveList", null); 
-        }
-        
+
         request.setAttribute("reserveList", reserveList); 
         request.setAttribute("visitedList", visitedList); 
 
         // 사용자 리스트 화면으로 이동(forwarding)
         return "/main/myPage.jsp";
     }
-    
+
     public static Date transformDate(String date)
     {
         SimpleDateFormat beforeFormat = new SimpleDateFormat("yyyy-mm-dd");
         SimpleDateFormat afterFormat = new SimpleDateFormat("yyyy-mm-dd");
-        
+
         java.util.Date tempDate = null;
-        
+
         try {
             tempDate = beforeFormat.parse(date);
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        
+
         String transDate = afterFormat.format(tempDate);
         Date d = Date.valueOf(transDate);
-        
+
         return d;
     }
 }
--- src/main/java/model/service/ReviewManager.java
+++ src/main/java/model/service/ReviewManager.java
@@ -58,4 +58,9 @@ public class ReviewManager {
     public ReviewDAO getreviewDAO() {
 		return this.reviewDAO;
 	}
+    
+    public boolean existingReview(int reservationId) throws SQLException {
+        return reviewDAO.existingReview(reservationId);
+    }
+    
 }
